560852c7b3d6170c51d7e3a2bfadd1a5e255f568
kiamaaa <victorkiama23@gmail.com>
2026-06-06 12:20:27 +0300

    updated

--- lib/book.py
+++ lib/book.py
@@ -1,23 +1,18 @@
-#!/usr/bin/env python3
-
 class Book:
     def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count
-
+        self._page_count = page_count
+    
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+    
     def turn_page(self):
-        print("Flipping the page...wow, you read fast")
-
-title_input = input("Enter book's title: ")
-pages_input = input("Enter page number: ")
-
-
-try:
-    page_count = int(pages_input)
-    
-    my_book = Book(title_input, page_count)
-    
-    my_book.turn_page()
-except ValueError:
-    print("page_count must be an integer")
-        
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-
 class Coffee:
-    def __init__(self,size,price):
-        self.size=size
-        self.price=price
-
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        valid_sizes = ["Small", "Medium", "Large"]
+        if value in valid_sizes:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+    
     def tip(self):
-        print("This coffee is great.here's a tip!")
-        self.price +=1
-
-size_input=input("Enter Coffee size:")  
-price_input=float(input("Enter Coffee price:"))      
-
-if size_input != "Small"  and size_input != "Medium" and size_input!= "Large":
-    print("size must be Small,Medium or Large")
-else:    
-    coffee_details=Coffee(size_input,price_input)
-    coffee_details.tip()
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
