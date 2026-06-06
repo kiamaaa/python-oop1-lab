fd3f73381b04954e1069704bd247fbaa269e57ab
kiamaaa <victorkiama23@gmail.com>
2026-06-06 12:00:54 +0300

    first commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast")
+
+title_input = input("Enter book's title: ")
+pages_input = input("Enter page number: ")
+
+
+try:
+    page_count = int(pages_input)
     
+    my_book = Book(title_input, page_count)
+    
+    my_book.turn_page()
+except ValueError:
+    print("page_count must be an integer")
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self,size,price):
+        self.size=size
+        self.price=price
+
+    def tip(self):
+        print("This coffee is great.here's a tip!")
+        self.price +=1
+
+size_input=input("Enter Coffee size:")  
+price_input=float(input("Enter Coffee price:"))      
+
+if size_input != "Small"  and size_input != "Medium" and size_input!= "Large":
+    print("size must be Small,Medium or Large")
+else:    
+    coffee_details=Coffee(size_input,price_input)
+    coffee_details.tip()
